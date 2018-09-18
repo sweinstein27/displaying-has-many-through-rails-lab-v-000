c04574df4a9d599fdf5b3e93e5d89a3b2304091e
Scott Weinstein <sweinstein27@gmail.com>
2018-09-18 17:10:46 +0000

    automatically backed up by learn

--- spec/features/patients_spec.rb
+++ spec/features/patients_spec.rb
@@ -1,5 +1,5 @@
 # require 'rails_helper'
-# 
+#
 # describe "patients", type:  :feature do
 #   before do
 #     Doctor.create([
@@ -9,7 +9,7 @@
 #       {name: "Phillip Chandler", department: "Internal Medicine"},
 #       {name: "Michaela Quinn", department: "Internal Medicine"}
 #     ])
-# 
+#
 #     Patient.create([
 #       {name: "Homer Simpson", age:38},
 #       {name: "Bart Simpson", age:10},
@@ -17,7 +17,7 @@
 #       {name: "Lisa Simpson", age:8},
 #       {name: "Maggie Simpson", age:1},
 #     ])
-# 
+#
 #     Appointment.create([
 #       {appointment_datetime: DateTime.new(2016, 03, 15, 18, 00, 0), patient_id: 1, doctor_id: 2},
 #       {appointment_datetime: DateTime.new(2016, 01, 11, 20, 20, 0), patient_id: 2, doctor_id: 1},
@@ -27,11 +27,11 @@
 #       {appointment_datetime: DateTime.new(2016, 05, 31, 17, 00, 0), patient_id: 1, doctor_id: 2},
 #       {appointment_datetime: DateTime.new(2017, 06, 03, 10, 00, 0), patient_id: 3, doctor_id: 1}
 #       ])
-# 
+#
 #       @meredith = Doctor.first
 #       @bart = Patient.find(2)
 #   end
-# 
+#
 #   describe "#index page" do
 #     it 'lists all of the patients and the number of appointments they have' do
 #       visit patients_path
@@ -41,17 +41,17 @@
 #       end
 #     end
 #   end
-# 
+#
 #   describe "#show page" do
 #     it "shows all of a patient's appointment times in a human readable format" do
 #       visit patient_path(@bart)
 #       expect(page).to have_content("January 11, 2016 at 20:20")
 #     end
-# 
+#
 #     it "links to the doctor's show page by name for the doctor of each appointment" do
 #       visit patient_path(@bart)
 #       expect(page).to have_link("Meredith Grey", href: doctor_path(@meredith))
 #     end
 #   end
-# 
+#
 # end
