7aad4b6e2d0e2fc5b223058519b326351f78ac40
Scott Weinstein <sweinstein27@gmail.com>
2018-09-18 17:11:01 +0000

    automatically backed up by learn

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -1,7 +1,7 @@
 # require 'rails_helper'
-# 
+#
 # describe 'Patient' do
-# 
+#
 #   before do
 #     @patient = Patient.create(name: "Russell Wilson", age: 27)
 #     @mcdreamy = Doctor.create(name: "Derek Shepherd", department: "Neurosurgery")
@@ -9,19 +9,19 @@
 #     @appointment1 = Appointment.create(appointment_datetime: DateTime.new(2016, 10, 1), doctor: @mcdreamy, patient: @patient  )
 #     @appointment2 = Appointment.create(appointment_datetime: DateTime.new(2016, 10, 1), doctor: @meredith, patient: @patient  )
 #   end
-# 
+#
 #   it 'has a name' do
 #     expect(@patient.name).to eq("Russell Wilson")
 #   end
-# 
+#
 #   it 'has an age' do
 #     expect(@patient.age).to eq(27)
 #   end
-# 
+#
 #   it 'has many appointments' do
 #     expect(@patient.appointments).to eq([@appointment1, @appointment2])
 #   end
-# 
+#
 #   it 'has many doctors, through appointments' do
 #     expect(@patient.doctors).to eq([@mcdreamy, @meredith])
 #   end
